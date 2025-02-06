39c4ba4ee146e901dd0bd58dd3cc4ad56f460bbb
Rogério Fernandes <rog.fernandes@yahoo.com.br>
2025-02-06 16:31:26 -0300

    Commit Inicial Testes

---
+++ source/scripts/exemplo.js
@@ -0,0 +1,4 @@
+// para poder importar um função exportada de outro arquivo JS
+import somar from './sum';
+
+console.log('somar: ', somar(10, 20));
---
+++ source/scripts/main.js
@@ -0,0 +1,6 @@
+function somar(a, b) {
+    return a + b;
+}
+
+// para poder exportar uma função para outro arquivo JS
+export default somar;
